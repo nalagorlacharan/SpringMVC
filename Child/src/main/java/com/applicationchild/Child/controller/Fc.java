6880dc42b285887098a194e1097e5be2819b5e04
nalagorlacharan <nalagorlacharan17@gmail.com>
2025-03-16 22:01:10 -0400

    version1

--- Child/src/main/java/com/applicationchild/Child/controller/Fc.java
+++ Child/src/main/java/com/applicationchild/Child/controller/Fc.java
@@ -1,5 +1,12 @@
 package com.applicationchild.Child.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class Fc {
+	
+	public String welcome() {
+		return "welcome";
+	}
 
 }
